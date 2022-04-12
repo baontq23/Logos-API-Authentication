4ce3c1e79a8eb1eac9ff7b835bbdbf91cb696e65
Bao Nguyen <baooshacker@gmail.com>
2022-04-13 01:25:03 +0700

    add API

---
+++ Auth/APIKey.h
@@ -0,0 +1,23 @@
+#import "UIKit/UIKit.h"
+#include <substrate.h>
+
+#pragma once
+
+@interface APIClient : UIView
++ (void)setEmail:(NSString*)email_;
++ (void)setDebid:(NSInteger)debid_;
++ (void)setDebVersion:(NSString*)version_;
++ (NSString *)getUserName;
++ (NSString *)getDeviceMessage;
++ (NSString *)getUUID;
++ (NSString *)getKey;
++ (NSString *)getDeviceModel;
++ (const char *)getKeyType;
++ (int)getDay;
++ (int) getidKey;
++ (instancetype)View;
++ (void)reloadInfo;
++ (void)paid:(void (^)(void))execute; //
++ (void)free:(void (^)(void))execute; //
++ (void)vipPaid:(void (^)(void))execute; //
+@end
