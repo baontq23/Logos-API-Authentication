8f0585a7598b7b77f04d6f2eb9e181c57be47f71
baontq23 <baooshacker@gmail.com>
2024-04-23 16:33:15 +0700

    5.4.2 lite secure

---
+++ API (Lite Secure)/APIClient.h
@@ -0,0 +1,26 @@
+//
+//  APIClient.h
+//  APIClient
+//
+//  Created by Bao Nguyen on 01/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APIClient : NSObject
+- (void) paid:(void (^)(void))execute;
+- (void) setToken:(NSString*)token;
+- (void) setLanguage:(NSString*)language;
+- (id)getPackageDataWithKey:(NSString*)key;
+- (NSString*) getKey;
+- (NSString*) getExpiryDate;
+- (NSString*) getUDID;
+- (NSString*) getDeviceModel;
++ (instancetype) sharedAPIClient;
+@end
+
+NS_ASSUME_NONNULL_END
+
