c081ca10965951084fe47c74ea3cda51e009d982
baontq23 <baooshacker@gmail.com>
2025-07-10 20:08:01 +0700

    update 5.5.0

--- API/APIClient.h
+++ API/APIClient.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (void) paid:(void (^)(void))execute;
 - (void)start:(void (^)(void))onStart init:(void (^)(void))init;
 - (void) setToken:(NSString*)token;
+- (void) setUDID:(NSString*)uid;
 - (void) setLanguage:(NSString*)language;
 - (void) hideUI:(bool)isHide;
 - (void) strictMode:(bool)_isStrictMode;
@@ -25,6 +26,9 @@ NS_ASSUME_NONNULL_BEGIN
 - (NSString*) getDeviceModel;
 - (NSString*) getLoginIP;
 - (NSString*) getPackageName;
+- (void) onCheckPackage:(void (^)(NSDictionary *header))success onFailure:(void (^)(NSDictionary *error))failure;
+- (void) onCheckDevice:(void (^)(NSDictionary *data))success onFailure:(void (^)(NSDictionary *error))failure;
+- (void) onLogin:(NSString *)inputKey onSuccess:(void (^)(NSDictionary *data))success onFailure:(void (^)(NSDictionary *error))failure;
 + (instancetype) sharedAPIClient;
 @end
 
