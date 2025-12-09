d7161ccd290cd179c3c77ffe6394afed3e8cdba5
baontq23 <baooshacker@gmail.com>
2025-12-09 11:32:58 +0700

    5.5.3

--- API (Lite Secure)/APIClient.h
+++ API (Lite Secure)/APIClient.h
@@ -12,15 +12,24 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface APIClient : NSObject
 - (void) paid:(void (^)(void))execute;
+- (void)start:(void (^)(void))onStart init:(void (^)(void))init;
 - (void) setToken:(NSString*)token;
+- (void) setUDID:(NSString*)uid;
 - (void) setLanguage:(NSString*)language;
+- (void) hideUI:(bool)isHide;
+- (void) strictMode:(bool)_isStrictMode;
+- (void) silentMode:(bool)_isSilentMode;
 - (id)getPackageDataWithKey:(NSString*)key;
 - (NSString*) getKey;
 - (NSString*) getExpiryDate;
+- (NSString*) getExpiredAt;
 - (NSString*) getUDID;
 - (NSString*) getDeviceModel;
 - (NSString*) getLoginIP;
 - (NSString*) getPackageName;
+- (void) onCheckPackage:(void (^)(NSDictionary *header))success onFailure:(void (^)(NSDictionary *error))failure;
+- (void) onCheckDevice:(void (^)(NSDictionary *data))success onFailure:(void (^)(NSDictionary *error))failure;
+- (void) onLogin:(NSString *)inputKey onSuccess:(void (^)(NSDictionary *data))success onFailure:(void (^)(NSDictionary *error))failure;
 + (instancetype) sharedAPIClient;
 @end
 
